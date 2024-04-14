5984ccc7770d4b4b9feb006dc7d8338fa90187d5
cedrotech1 <cedrotech1@gmail.com>
2024-04-14 14:36:49 +0200

    email

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the HTML form
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Handle form submission
+app.post('/send-email', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'cedrickhakuzimana@gmail.com',
+            pass: 'twtu kezj kneg opjb'
+        }
+    });
+
+    const mailOptions = {
+        from: 'cedrickhakuzimana@gmail.com',
+        to: req.body.to,
+        subject: req.body.subject,
+        html: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Email sent successfully');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
